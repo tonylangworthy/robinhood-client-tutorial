6a802a9794cbbb1e8e6e5bc5304471dad5bd4f6a
Tony Langworthy <tony.langworthy@hotmail.com>
2020-03-17 15:09:00 -0500

    Tested RestTemplate

--- src/main/java/com/autotrader/web/client/robinhood/authorization/RetrieveToken.java
+++ src/main/java/com/autotrader/web/client/robinhood/authorization/RetrieveToken.java
@@ -1,8 +1,16 @@
 package com.autotrader.web.client.robinhood.authorization;
 
+import org.springframework.web.client.RestTemplate;
+
 public class RetrieveToken {
 
+    private RestTemplate restTemplate;
+
+    public RetrieveToken(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     public void invoke() {
-        
+        System.out.println("\n\n\nIs restTemplate null? " + (restTemplate == null));
     }
 }
--- src/test/java/com/autotrader/web/client/robinhood/authorization/RetrieveTokenTest.java
+++ src/test/java/com/autotrader/web/client/robinhood/authorization/RetrieveTokenTest.java
@@ -1,13 +1,28 @@
 package com.autotrader.web.client.robinhood.authorization;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
+@RunWith(SpringRunner.class)
 public class RetrieveTokenTest {
 
-    private RetrieveToken retrieveToken = new RetrieveToken();
+    private RetrieveToken retrieveToken;
+
+    @Mock
+    private RestTemplate mockRestTemplate;
+
 
     @Test
     public void invokeTest() {
         retrieveToken.invoke();
+    }
+
+    @Before
+    public void setup() {
+        retrieveToken = new RetrieveToken(mockRestTemplate);
     }
 }
